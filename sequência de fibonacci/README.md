88569307a3fadff33578d41e2999f175dc25673a
Elton Possidonio <70237633+eltonrp@users.noreply.github.com>
2020-10-28 17:56:05 -0300

    fibo

---
+++ sequência de fibonacci/README.md
@@ -0,0 +1,9 @@
+#Sequência de Fibonacci:
+##Sequência ou Sucessão de Fibonacci
+* Para n-ésima posição:
+    ** 0 para n ==0;
+    ** 1 para n == 1;
+    ** f(n-1) + f(n-2) para n >= 2
+
+Para usar a função, basta passar o parâmetro n em fibo(n).
+A função faz uso da Recursão para retornar o valor da posição desejada.
---
+++ sequência de fibonacci/fibonacci.py
@@ -0,0 +1,12 @@
+def fibo(n):
+    """
+    :param n: parâmetro que indica a posição desejada
+    :return: retorna o valor da posição desejada
+    """
+    if n == 0:  # ---> Pos 0 == 0
+        return 0
+    elif n == 1:  # ---> Pos 1 == 1
+        return 1
+    else:
+        res = fibo(n - 1) + fibo(n - 2)
+        return res
