02bfc2d7d7c22c7cf1fbe9443e5f310cdc9a1cf6
Elton Possidonio <70237633+eltonrp@users.noreply.github.com>
2020-11-03 20:13:00 -0300

    exercicío fibonacci

--- sequência de fibonacci/README.md
+++ sequência de fibonacci/README.md
@@ -5,5 +5,11 @@
     * 1 para n == 1;
     * f(n-1) + f(n-2) para n >= 2
 
-Para usar a função, basta passar o parâmetro n em fibo(n).  
-A função faz uso da **Recursão** para retornar o valor da posição desejada.
+Nesse exercício podemos ver como o tempo de processamento é afetado com o uso da **Recurssão** e como podemos amenizar esse problema, com a técnica da **Memoização**, quando usando a **Recurssão** .
+O programa faz uso de três funções: 
+* f_rec(n) ---> função **recursiva** apenas,
+* f_mem(n) ---> função **recursiva com memoização**,
+* f_it(n) ---> função **iterativa**.
+
+Altere a variável **num** e veja a diferença de tempo de processamento entre elas. Use sempre inteiros positivos e comece com números baixos.  
+Dica: A partir de 30, o processamento da função recursiva começa a demorar mais de segundos.
--- sequência de fibonacci/fibonacci.py
+++ sequência de fibonacci/fibonacci.py
@@ -1,12 +1,41 @@
-def fibo(n):
-    """
-    :param n: parâmetro que indica a posição desejada
-    :return: retorna o valor da posição desejada
-    """
-    if n == 0:  # ---> Pos 0 == 0
-        return 0
-    elif n == 1:  # ---> Pos 1 == 1
-        return 1
+import time
+
+
+def f_rec(n):
+    if n < 2:
+        return n
     else:
-        res = fibo(n - 1) + fibo(n - 2)  # ---> Uso da Recursão  
+        res = f_rec(n - 1) + f_rec(n - 2)
         return res
+
+
+def f_mem(n):
+    if n < 2:
+        return n
+    elif m.get(n) != None:
+        return m[n]
+    else:
+        m[n] = f_mem(n - 1) + f_mem(n - 2)
+        return m[n]
+
+
+def f_it(n):
+    res = n
+    a, b = 0, 1
+    for e in range(2, n + 1):
+        res = a + b
+        a, b = b, res
+    return res
+
+
+m = {}
+num = 35
+start = time.time()
+print(f_it(num))
+print(f'Iterativa: {time.time() - start}')
+start = time.time()
+print(f_mem(num))
+print(f'Memoização: {time.time() - start}')
+start = time.time()
+print(f_rec(num))
+print(f'Recursiva: {time.time() - start}')
